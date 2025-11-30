0cde596cfbf7b2a2fe90518dca7a7b9e6318576d
Sumant Kumar <54070767+sumant8521@users.noreply.github.com>
2025-11-30 17:38:11 +0000

    Dom learning

---
+++ 04_basic/01_forin.js
@@ -0,0 +1,19 @@
+const myObject = {
+    js: "Javascript",
+    cpp: "C++",
+    rb: "ruby",
+    swift: "Swift by apple"
+}
+
+// for (const key in myObject) {
+//     console.log(key);
+    
+    
+// }
+
+for (const key in myObject) {
+
+    console.log(myObject[key]);
+    
+    
+}
